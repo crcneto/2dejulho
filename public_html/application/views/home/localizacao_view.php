924fe50bfa39812dcfa12e4346abf973ceec4cbf
neto <neto@netobook>
2017-06-23 17:49:00 -0300

    Primeiro Commit

---
+++ public_html/application/views/home/localizacao_view.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <h1 class="text-center">Localização</h1>
+    <br>
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d861.4830740756071!2d-48.72729809016015!3d-26.955722831532967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1spt-BR!2sbr!4v1498051212482" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+</div>
