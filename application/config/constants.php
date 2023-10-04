3f05870860355128a49fbdb0b87a27f13e30c8c2
7desenvolvedor <7desen@cbm.sc.gov.br>
2023-10-04 11:17:09 -0300

    reconfig all - testing server

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,4 +85,4 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 //comutador AMBIENTE=>1 = SERVER / AMBIENTE=>0 = LOCAL
-define('AMBIENTE', 0);
+define('AMBIENTE', 1);
